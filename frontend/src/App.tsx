fc84bcc786d43f4b6f73c19d3677ce10e3aabb16
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2025-06-16 16:33:22 +0700

    Update_routeinapptsx
    adding one more route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'react-calendar/dist/Calendar.css';
 import LoginPage from "./pages/Login/LoginPage";
 import UserDashboard from "./pages/Dashboard/UserDashboard";
+import KnowledgeForumLayout from "./pages/Forum/KnowledgeForumLayout";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/forum" element={<KnowledgeForumLayout />} />
       </Routes>
     </Router>
   );
