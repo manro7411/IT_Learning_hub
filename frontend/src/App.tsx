4a4b5dd501d083e726fe8d2882440b9b307afb65
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2025-06-16 16:56:45 +0700

    Update_route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import 'react-calendar/dist/Calendar.css';
 import LoginPage from "./pages/Login/LoginPage";
 import UserDashboard from "./pages/Dashboard/UserDashboard";
 import KnowledgeForumLayout from "./pages/Forum/KnowledgeForumLayout";
+import PointDashboard from "./pages/Point/PointDashboard";
+import Gamedashboard from "./pages/Game/Gamedashboard";
+import Lessondashboard from "./pages/Lesson/Lessondashboard";
 
 function App() {
   return (
@@ -10,7 +13,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/lesson" element={<Lessondashboard />} />
+        <Route path="/task" element={<Lessondashboard />} />
         <Route path="/forum" element={<KnowledgeForumLayout />} />
+        <Route path="/point" element={<PointDashboard />} />
+        <Route path="/game" element={<Gamedashboard />} />
+        <Route path="/game" element={<Gamedashboard />} />
       </Routes>
     </Router>
   );
