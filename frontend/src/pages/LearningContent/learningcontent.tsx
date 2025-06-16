7e871fe17b2160365a11834232415080a7413370
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2025-06-16 22:39:28 +0700

    Update_learningcontent

--- frontend/src/pages/LearningContent/learningcontent.tsx
+++
@@ -1,9 +0,0 @@
-const LearningContent = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Learning Content</h1>
-      <p className="text-lg">This is the learning content page.</p>
-    </div>
-  );
-}
-export default LearningContent;
