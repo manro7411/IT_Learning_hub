72253e48c85e98a2ad7ff09aa1f671965b852ba0
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2025-06-16 16:57:54 +0700

    Create_Lessendashboard

---
+++ frontend/src/pages/Lesson/Lessondashboard.tsx
@@ -0,0 +1,9 @@
+const Lessondashboard = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">Lesson Dashboard</h1>
+        <p className="text-lg">This is the lesson dashboard page.</p>
+        </div>
+    );
+    }
+export default Lessondashboard;
