fee81081b591038779378f48ac27cb9efab72cd4
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2025-06-18 14:26:33 +0700

    Update_userObj

--- backend/src/main/java/model/User.java
+++ backend/src/main/java/model/User.java
@@ -1,15 +1,67 @@
 package model;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-public class User {
+@Table(name = "users", schema = "authentication")
+public class User{
     @Id
-    @GeneratedVaue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
+    private String name;
+    @Column(unique = true, nullable = false)
     private String email;
-    private String role;
+    @Column(nullable = false)
+    private String password;
+    private String role = "employee";
+    @Column(name = "created_at")
+    private LocalDateTime created;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
 }
